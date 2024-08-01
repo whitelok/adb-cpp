f73a1f85050142136c8c338fe73de8478812b7df
karll <karlluo@tencent.com>
2024-08-01 09:54:46 +0800

    debug

---
+++ adb_cpp_client/adb_cpp_client.cc
@@ -0,0 +1,11 @@
+#include <cstdio>
+
+#include <stdio.h>
+
+#include "adb/adb.h"
+#include "adb/adb_client.h"
+#include "adb/sysdeps.h"
+
+int main(int argc, char* argv[]) {
+    adb_sysdeps_init();
+}
